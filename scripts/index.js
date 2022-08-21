2db0a2d40facf06e2260a653294a6db5b26d747c
george-elamir <george.elamir24@gmail.com>
2022-08-21 06:30:56 +0200

    feat: add functional tabs for courses section - Tabs filter the courses based on a its type - Tabs resets the searchbar input to eleminate any possible conflicts between both components - The searchbar now searchs for titles in the active tab

--- scripts/index.js
+++ scripts/index.js
@@ -6,29 +6,33 @@ window.onload = async function (e) {
   data = await data.json();
   const courses = data.courses;
 
+  let activetab = document.querySelectorAll(".nav-item .active");
+  let tabcourses = courses.filter((elem) => {
+    return elem.type === activetab[0].innerText;
+  });
+
   /**
    * DOM Manipulations
    */
   const cardsContainer = document.getElementById("cards-container");
   let cards = [];
-  courses.forEach((element,idx) => {
-
+  tabcourses.forEach((element, idx) => {
     const cardDiv = document.createElement("div");
     cardDiv.classList.add("carousel-item");
-    if(!idx) cardDiv.classList.add("active");
+    if (!idx) cardDiv.classList.add("active");
 
     const cardCol = document.createElement("div");
     cardCol.classList.add("col-md-3");
 
     const cardContainerTop = document.createElement("div");
     cardContainerTop.classList.add("card");
-    cardContainerTop.style.width="18rem";
+    cardContainerTop.style.width = "18rem";
 
     const cardContainer = document.createElement("div");
     cardContainer.classList.add("card-body");
 
     const cardImg = document.createElement("img");
-    cardImg.classList.add("card-img-top")
+    cardImg.classList.add("card-img-top");
     cardImg.setAttribute("src", element.image);
 
     const cardTitle = document.createElement("h4");
@@ -52,26 +56,56 @@ window.onload = async function (e) {
     cardContainer.appendChild(cardAuthor);
     cardContainer.appendChild(cardPrice);
 
-    cardContainerTop.appendChild(cardContainer)
+    cardContainerTop.appendChild(cardContainer);
     cardCol.appendChild(cardContainer);
     cardDiv.appendChild(cardCol);
     cardsContainer.appendChild(cardDiv);
     cards.push(cardDiv);
   });
+  let items = document.querySelectorAll(".carousel .carousel-item");
+
+  items.forEach((el) => {
+    const minPerSlide = 4;
+    let next = el.nextElementSibling;
+    for (var i = 1; i < minPerSlide; i++) {
+      if (!next) {
+        // wrap carousel by using first child
+        next = items[0];
+      }
+      let cloneChild = next.cloneNode(true);
+      el.appendChild(cloneChild.children[0]);
+      next = next.nextElementSibling;
+    }
+  });
+
   const searchbar = document.getElementById("searchbar");
   searchbar.addEventListener("input", (e) => {
     const searchRegex = new RegExp(e.target.value, "i");
 
     cardsContainer.textContent = "";
-    const filteredCourses = courses.filter((elem) => {
+    console.log(cardsContainer);
+    const filteredCourses = tabcourses.filter((elem) => {
       return searchRegex.test(elem.course_title);
     });
-    //console.log(filteredCourses);
-    filteredCourses.forEach((element) => {
+    console.log(filteredCourses);
+
+    filteredCourses.forEach((element, idx) => {
       const cardDiv = document.createElement("div");
-      cardDiv.classList.add("course-card");
+      cardDiv.classList.add("carousel-item");
+      if (!idx) cardDiv.classList.add("active");
+
+      const cardCol = document.createElement("div");
+      cardCol.classList.add("col-md-3");
+
+      const cardContainerTop = document.createElement("div");
+      cardContainerTop.classList.add("card");
+      cardContainerTop.style.width = "18rem";
+
+      const cardContainer = document.createElement("div");
+      cardContainer.classList.add("card-body");
 
       const cardImg = document.createElement("img");
+      cardImg.classList.add("card-img-top");
       cardImg.setAttribute("src", element.image);
 
       const cardTitle = document.createElement("h4");
@@ -90,29 +124,104 @@ window.onload = async function (e) {
       cardPrice.classList.add("font-bold");
       cardPrice.appendChild(priceText);
 
-      cardDiv.appendChild(cardImg);
-      cardDiv.appendChild(cardTitle);
-      cardDiv.appendChild(cardAuthor);
-      cardDiv.appendChild(cardPrice);
+      cardContainer.appendChild(cardImg);
+      cardContainer.appendChild(cardTitle);
+      cardContainer.appendChild(cardAuthor);
+      cardContainer.appendChild(cardPrice);
 
+      cardContainerTop.appendChild(cardContainer);
+      cardCol.appendChild(cardContainer);
+      cardDiv.appendChild(cardCol);
       cardsContainer.appendChild(cardDiv);
       cards.push(cardDiv);
     });
-  });
-  let items = document.querySelectorAll('.carousel .carousel-item')
+    let items = document.querySelectorAll(".carousel .carousel-item");
 
-items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
+    items.forEach((el) => {
+      const minPerSlide = 4;
+      let next = el.nextElementSibling;
+      for (var i = 1; i < minPerSlide; i++) {
         if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
+          // wrap carousel by using first child
+          next = items[0];
+        }
+        let cloneChild = next.cloneNode(true);
+        el.appendChild(cloneChild.children[0]);
+        next = next.nextElementSibling;
+      }
+    });
+  });
 
+  document.querySelectorAll(".nav-link").forEach((node) => {
+    node.addEventListener("click", () => {
+      searchbar.value = "";
+      cardsContainer.textContent = "";
+      activetab = document.querySelectorAll(".nav-item .active");
+      console.log(activetab[0]);
+      tabcourses = courses.filter((elem) => {
+        return elem.type === activetab[0].innerText;
+      });
+      tabcourses.forEach((element, idx) => {
+        const cardDiv = document.createElement("div");
+        cardDiv.classList.add("carousel-item");
+        if (!idx) cardDiv.classList.add("active");
+
+        const cardCol = document.createElement("div");
+        cardCol.classList.add("col-md-3");
+
+        const cardContainerTop = document.createElement("div");
+        cardContainerTop.classList.add("card");
+        cardContainerTop.style.width = "18rem";
+
+        const cardContainer = document.createElement("div");
+        cardContainer.classList.add("card-body");
+
+        const cardImg = document.createElement("img");
+        cardImg.classList.add("card-img-top");
+        cardImg.setAttribute("src", element.image);
+
+        const cardTitle = document.createElement("h4");
+        const titleText = document.createTextNode(element.course_title);
+        cardTitle.classList.add("card-title");
+        cardTitle.appendChild(titleText);
+
+        const cardAuthor = document.createElement("p");
+        const authorText = document.createTextNode(element.author);
+        cardAuthor.classList.add("card-author");
+        cardAuthor.appendChild(authorText);
+
+        const cardPrice = document.createElement("div");
+        const priceText = document.createTextNode(element.price);
+        cardPrice.classList.add("card-price");
+        cardPrice.classList.add("font-bold");
+        cardPrice.appendChild(priceText);
+
+        cardContainer.appendChild(cardImg);
+        cardContainer.appendChild(cardTitle);
+        cardContainer.appendChild(cardAuthor);
+        cardContainer.appendChild(cardPrice);
+
+        cardContainerTop.appendChild(cardContainer);
+        cardCol.appendChild(cardContainer);
+        cardDiv.appendChild(cardCol);
+        cardsContainer.appendChild(cardDiv);
+        cards.push(cardDiv);
+      });
+      let items = document.querySelectorAll(".carousel .carousel-item");
+
+      items.forEach((el) => {
+        const minPerSlide = 4;
+        let next = el.nextElementSibling;
+        for (var i = 1; i < minPerSlide; i++) {
+          if (!next) {
+            // wrap carousel by using first child
+            next = items[0];
+          }
+          let cloneChild = next.cloneNode(true);
+          el.appendChild(cloneChild.children[0]);
+          next = next.nextElementSibling;
+        }
+      });
+    });
+  });
 };
